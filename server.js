604c5606cbf02d6e7104b86b3bad6ce891e0f4ea
System <none>
2017-07-24 23:36:37 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,39 +1,91 @@
 // server.js
 // where your node app starts
 
-// init project
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
+var Schema = mongoose.Schema;
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var host = process.env.IP || 'localhost';
+var port = process.env.PORT || 80;
+var mongoURI = "mongodb://user:password@ds111123.mlab.com:11123/image-parser-microservice";
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+function GetHash(url) {
+  var hash = crypto.createHash('md5');
+  hash.update(url);
+  return hash.digest('hex');
+}
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+/**
+ * Connect to Mongodb
+ */
+MongoClient.connect(mongoURI, function(err, db) {
+  if(err) { 
+    console.log(err); 
+  }
+  
+  var collection = db.collection("url-map");
+  
+  app.get('/:URL', function(req, res) {
+    console.log(req.params.URL);
+    collection.findOne({hash: req.params.URL.toString() }
+      , function(err, item) {
+      if (err) {
+        console.log(err);
+        res.writeHead(420);
+        res.end("error retrieving short url from database");
+        return;
+      }
+      
+      console.log(item.url);
+      
+      res.redirect(item.url);
+    });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
+  
+  });
+  
+  app.get('/new/*', function(req, res) {
+    console.log(req.params[0]);
+    // return error for invalid url
+    if (!url.parse(req.params[0]).host){
+      res.writeHead(420);
+      res.end("error- malformed url");
+      return;
+    }
+    
+    var hash = GetHash(req.params[0]);
+    
+    // store url
+    collection.insert({'url': req.params[0], 'hash': hash}
+      , {w:1}
+      , function(err, result) {
+      if (err) {
+        res.writeHead(400);
+        console.log(err);
+        res.end("error inserting url into databse");
+        return;
+      }
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+      console.log(JSON.stringify({'newUrl': hash}));
+      
+      // return json
+      res.writeHead(200);
+      res.end(JSON.stringify({'newUrl': hash}));
+    });
+  });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+})
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  
+/**
+ * Listen for requests
+ */
+app.listen(port, function(err, data) {
+  if (err) {
+    console.log("Error starting express on port " + port)
+    console.log(err);
+  }
+  console.log("express on port " + port);
 });
