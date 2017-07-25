8c2f4f880657fe1d67abea45a7b04f29165df167
System <none>
2017-07-25 00:52:37 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const 
 
 const app = express();
 const host = process.env.IP || 'localhost';
@@ -10,25 +11,31 @@ function start(collection) {
   });
 
   app.get("/latest", (request, response) => {
-    collection.find({}
-      , function(err, item) {
-        if (err) {
-          console.log(err);
-          response.writeHead(420);
-          response.end("error retrieving short url from database");
-          return;
-        }
+    collection
+      .find({})
+      .limit(10)
+      .toArray(function(err, item) {
+          if (err) {
+            console.log(err);
+            response.writeHead(420);
+            response.end("error retrieving short url from database");
+            return;
+          }
 
-        console.log(item);
+          console.log(item);
 
-        response.end(JSON.stringify(item));
-      });
+          response.end(JSON.stringify(item));
+        });
   });
 
 
   app.get("/imagesearch/:query", (request, response) => {
     const search_term = request.params.query.toString();
     const offset = request.query.offset;
+    collection.insertOne({
+        term: search_term
+       , when: (new Date()).toISOString() 
+      });
 
     response.end(request.params.query.toString()+ " query str " + request.query.offset);
   });
