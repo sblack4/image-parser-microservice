bd53cff8de660da80141aa855ff1df09d866453f
System <none>
2017-07-25 01:03:37 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const 
+const bing = require('bing.search');
 
 const app = express();
 const host = process.env.IP || 'localhost';
 const port = process.env.PORT || 80;
 
+/**
+ * express app as an exportable function
+ */
 function start(collection) {
+  
+  // home route
   app.get("/", (request, response) => {
     response.end("try going to /latest");
   });
 
+  // get last ten searches
   app.get("/latest", (request, response) => {
     collection
       .find({})
@@ -28,21 +34,42 @@ function start(collection) {
         });
   });
 
-
+  // search for a particular query
   app.get("/imagesearch/:query", (request, response) => {
     const search_term = request.params.query.toString();
     const offset = request.query.offset;
+    
+    // insert search into history
     collection.insertOne({
         term: search_term
        , when: (new Date()).toISOString() 
       });
+    
+    const search = new bing(process.env.API_KEY);
+    // Query the image and populate results
+    search.images(search_term, {
+        top: offset
+      },
+      function(err, results) {
+        if (err) throw err;
+        response.send(results.map(makeList));
+      }
+    );
 
+
+    function makeList(img) {
+      // Construct object from the json result
+      return {
+        "url": img.url,
+        "snippet": img.title,
+        "thumbnail": img.thumbnail.url,
+        "context": img.sourceUrl
+      };
+    }
     response.end(request.params.query.toString()+ " query str " + request.query.offset);
   });
 
-  /**
-   * Listen for requests
-   */
+  // Listen for requests
   app.listen(port, function(err, data) {
     if (err) {
       console.log("Error starting express on port " + port);
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var MongoClient = require('mongodb').MongoClient;
 const app = require("./app.js");
 
 // globals 
-const mongoURI = "mongodb://user:password@ds111123.mlab.com:11123/image-parser-microservice";
+const mongoURI = "mongodb://" + process.env.SECRET + "@ds111123.mlab.com:11123/image-parser-microservice";
 var db;
 var collection;
 
