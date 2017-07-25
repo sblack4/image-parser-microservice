241ef2e14bbf64d9d7b8b014448425d99d92dde9
System <none>
2017-07-25 00:30:37 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,55 +1,21 @@
 const express = require("express");
 
 const app = express();
-var host = process.env.IP || 'localhost';
-var port = process.env.PORT || 80;
-
-function start(History) {
   
-  app.get("/", (request, response) => {
-    response.end("try going to /latest");
-  });
-  
-  app.get("/latest", (request, response) => {
-  function getHistory(req, res) {
-    // Check to see if the site is already there
-    History.find({}, null, {
-      "limit": 10,
-      "sort": {
-        "when": -1
-      }
-    }, function(err, history) {
-      if (err) return console.error(err);
-      console.log(history);
-      res.send(history.map(function(arg) {
-        // Displays only the field we need to show.
-        return {
-          term: arg.term,
-          when: arg.when
-        };
-      }));
-    });
-  }
-  });
+app.get("/", (request, response) => {
+  response.end("try going to /latest");
+});
+
+app.get("/latest", (request, response) => {
+
+});
 
 
-  app.get("/imagesearch/:query", (request, response) => {
-    const search_term = request.params.query.toString();
-    const offset = request.query.offset;
-    
-    response.end(request.params.query.toString()+ " query str " + request.query.offset);
-  });
+app.get("/imagesearch/:query", (request, response) => {
+  const search_term = request.params.query.toString();
+  const offset = request.query.offset;
 
-  /**
-   * Listen for requests
-   */
-  app.listen(port, function(err, data) {
-    if (err) {
-      console.log("Error starting express on port " + port);
-      console.log(err);
-    }
-    console.log("express on port " + port);
-  });
-}
+  response.end(request.params.query.toString()+ " query str " + request.query.offset);
+});
 
-module.exports = start;
+module.exports = app;
--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 
 // imports 
 const mongodb = require('mongodb');
-const express_app = require("./app.js");
+const app = require("./app.js");
 
 // globals 
 const host = process.env.IP || 'localhost';
 const port = process.env.PORT || 80;
 const mongoURI = "mongodb://user:password@ds111123.mlab.com:11123/image-parser-microservice";
 
-const history_schema = new Schema({
-  term: String,
-  when: String
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
+// Initialize connection once
+MongoClient.connect(mongoURI, function(err, database) {
+  if(err) throw err;
+
+  db = database;
+
+  /**
+   * Listen for requests
+   */
+  app.listen(port, function(err, data) {
+    if (err) {
+      console.log("Error starting express on port " + port);
+      console.log(err);
+    }
+    console.log("express on port " + port);
+  });
 });
-
-const History = mongoose.model('History', history_schema);
-mongoose.connect(mongoURI);
-
-express_app(History);
